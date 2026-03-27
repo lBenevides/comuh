9bd8b2999c94b0406cf90da0761ec559eb3383e2
Lucas Benevides <lhenriquebenevides@gmail.com>
2026-03-27 12:58:23 -0300

    adicionado ignore de classes base do rails para o simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ SimpleCov.start "rails" do
   add_filter "/spec/"
   add_filter "/config/"
   add_filter "/vendor/"
+  add_filter "/app/jobs/application_job.rb"
+  add_filter "/app/mailers/application_mailer.rb"
 end
 
 RSpec.configure do |config|
